d67a6be1fcc7f6c0f2b6a46a2ce73fe84a3ad6f0
cattuong28597 <cattuong28597@gmail.com>
2021-09-13 09:25:28 +0700

    lan1

---
+++ module2/src/com/codegym/week2/day1/DeleteElementInArray.java
@@ -0,0 +1,46 @@
+package com.codegym.week2.day1;
+
+import java.util.Scanner;
+
+public class DeleteElementInArray {
+    public static void main(String[] args) {
+        int size;
+        int [] array;
+        Scanner input = new Scanner(System.in);
+        do {
+            System.out.print("Enter a size:");
+            size = input.nextInt();
+            if (size > 20)
+                System.out.println("Size should not exceed 20");
+        } while (size > 20);
+        array = new int[size];
+        int i = 0;
+        while (i < array.length) {
+            System.out.print("Enter element " + (i + 1) + " : ");
+            array[i] = input.nextInt();
+            i++;
+        }
+        System.out.println("Property list: ");
+        for (int j = 0; j < array.length; j++) {
+            System.out.println(array[j] + "\t");
+        }
+        int X = 3;
+        int index = -1;
+        for (int a = 0; a < array.length; a++) {
+            if (array[a] == X) {
+                index = a;
+                break;
+            }
+        }
+        if (index != -1) {
+            for (int b = index; b < array.length-1; b++) {
+                array[b] = array[b + 1];
+            }
+            array[array.length-1] = 0;
+        }
+        System.out.println("New array: ");
+        for (int c = 0; c < array.length-1; c++) {
+            System.out.println(array[c] +"  ");
+        }
+    }
+}
