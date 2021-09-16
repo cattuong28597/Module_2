d54fcbdbc9354fb5550c1ce50918f1a262ed0aa2
cattuong28597 <cattuong28597@gmail.com>
2021-09-16 17:06:49 +0700

    lan1

---
+++ module2/src/com/codegym/week2/day5/Interface/CircleComparator.java
@@ -0,0 +1,15 @@
+package com.codegym.week2.day5.Interface;
+import java.util.Comparator;
+public class CircleComparator implements Comparator<Circle>{
+    @Override
+    public int compare(Circle c1, Circle c2) {
+        if (c1.getRadius() > c2.getRadius()) {
+            return 1;
+        }
+        else if (c1.getRadius() < c2.getRadius()) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+}
