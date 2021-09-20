3995ed53364a0504c8e2d72fd30963235ca5cde6
cattuong28597 <cattuong28597@gmail.com>
2021-09-20 11:01:35 +0700

    lan1

---
+++ module2/src/com/codegym/week3/day2/SimpleCalculator/Calculator.java
@@ -0,0 +1,11 @@
+package com.codegym.week3.day2.SimpleCalculator;
+
+public class Calculator {
+    public static int add(int first, int second){
+        return first + second;
+    }
+
+    public static int sub(int first, int second){
+        return first - second;
+    }
+}
