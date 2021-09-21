5bd4c0ba2f92673d369ea535f0550ee3166eb898
cattuong28597 <cattuong28597@gmail.com>
2021-09-21 10:06:34 +0700

    lan1

--- module2/src/com/codegym/week3/day2/TriangleClassifier/TriangleClassifier.java
+++ module2/src/com/codegym/week3/day2/TriangleClassifier/TriangleClassifier.java
@@ -1,2 +1,25 @@
-package com.codegym.week3.day2.TriangleClassifier;public class TriangleClassifier {
+package com.codegym.week3.day2.TriangleClassifier;
+
+public class TriangleClassifier {
+    public static String triangleClassifier(int side1, int side2, int side3) {
+        String triangleClassifier = "";
+        boolean isTriangle = side1 + side2 >= side3 && side2 + side3 >= side1 && side1 + side3 >= side2;
+        boolean isIsoscelesTriangle = side1 == side2 || side1 == side3 || side2 == side3;
+        boolean isEquilateralTriangle = side1 == side2 && side1 == side3;
+        boolean isRightTriangle = (side1 * side1 + side2 * side2) == side3 * side3 || (side1 * side1 + side3 * side3) == side2 * side2 || (side3 * side3 + side2 * side2) == side1 * side1;
+        if (isTriangle) {
+            if (isEquilateralTriangle) {
+                triangleClassifier = "This is a Equilateral Triangle";
+            } else if (isIsoscelesTriangle) {
+                triangleClassifier = "This is a Isosceles Triangle";
+            } else if (isRightTriangle) {
+                triangleClassifier = "This is a Right Triangle";
+            } else {
+                triangleClassifier = "This is a Triangle";
+            }
+        } else {
+            triangleClassifier = "This is not a Triangle";
+        }
+        return triangleClassifier;
+    }
 }
