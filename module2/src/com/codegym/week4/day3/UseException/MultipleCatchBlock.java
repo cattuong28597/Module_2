eb9d738f98cf190adea80574f7db80fa9e4bdf30
cattuong28597 <cattuong28597@gmail.com>
2021-10-06 08:56:44 +0700

    lan1

---
+++ module2/src/com/codegym/week4/day3/UseException/MultipleCatchBlock.java
@@ -0,0 +1,2 @@
+package com.codegym.week4.day3.UseException;public class MultipleCatchBlock {
+}
---
+++ module2/src/com/codegym/week4/day5/CopyFile/Main.java
@@ -0,0 +1,43 @@
+package com.codegym.week4.day5.CopyFile;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+
+public class Main {
+    private static void copyFileUsingJava7Files(File source, File dest) throws IOException {
+        Files.copy(source.toPath(), dest.toPath(), StandardCopyOption.REPLACE_EXISTING);
+    }
+
+    private static void copyFileUsingStream(File source, File dest) throws IOException {
+        InputStream is = null;
+        OutputStream os = null;
+        try {
+            is = new FileInputStream(source);
+            os = new FileOutputStream(dest);
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = is.read(buffer)) > 0) {
+                os.write(buffer, 0, length);
+            }
+        } finally {
+            is.close();
+            os.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        String sourcePath = "D:\\Module_2\\module2\\src\\com\\codegym\\week4\\day5\\CopyFile\\Sr.txt";
+        String destPath = "D:\\Module_2\\module2\\src\\com\\codegym\\week4\\day5\\CopyFile\\Ds.txt";
+        File sourceFile = new File(sourcePath);
+        File destFile = new File(destPath);
+
+        try {
+            copyFileUsingJava7Files(sourceFile, destFile);
+            System.out.printf("Copy Complete");
+        } catch (IOException e) {
+            System.out.printf("Can't copy that file");
+            System.out.printf(e.getMessage());
+        }
+    }
+}
