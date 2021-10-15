c7d228d4b1c060dad1cfcc21c2b71968bc0ccfe0
cattuong28597 <cattuong28597@gmail.com>
2021-10-15 11:01:00 +0700

    lan1

---
+++ module2/src/com/codegym/week5/day5/Strategy/QuickSort.java
@@ -0,0 +1,10 @@
+package com.codegym.week5.day5.Strategy;
+
+import java.util.List;
+
+public class QuickSort implements SortStrategy {
+     @Override
+    public <T> void sort(List<T> items) {
+         System.out.println("Quick sort");
+     }
+}
---
+++ module2/src/com/codegym/week5/day5/Strategy/SortStrategy.java
@@ -0,0 +1,7 @@
+package com.codegym.week5.day5.Strategy;
+
+import java.util.List;
+
+public interface SortStrategy {
+    <T> void sort(List<T> items);
+}
