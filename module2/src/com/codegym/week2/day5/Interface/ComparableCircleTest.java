ef1931a24952d1981389e1a772f02bf7cce9c661
cattuong28597 <cattuong28597@gmail.com>
2021-10-19 16:33:07 +0700

    lan1

--- module2/src/com/codegym/week2/day5/Interface/ComparableCircleTest.java
+++ module2/src/com/codegym/week2/day5/Interface/ComparableCircleTest.java
@@ -1,8 +1,9 @@
 package com.codegym.week2.day5.Interface;
 
 import java.util.Arrays;
+import java.util.Collection;
 
-public class ComparableCircleTest {
+public class ComparableCircleTest  {
     public static void main(String[] args) {
         ComparableCircle[] circles = new ComparableCircle[3];
         circles[0] = new ComparableCircle(3.6);
@@ -16,10 +17,14 @@ public class ComparableCircleTest {
 
         Arrays.sort(circles);
 
+
         System.out.println("After-sorted:");
         for (ComparableCircle circle : circles) {
             System.out.println(circle);
         }
+//        for (int i = circles.length-1; i >= 0; i--) {
+//            System.out.println(circles[i]);
+//        }
     }
     }
 
